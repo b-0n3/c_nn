9f0638514b3f1cd89e8a793bb954bf8fd98bd319
lalouabd <lalouabd123@gmail.com>
2020-12-24 02:16:50 +0100

    add matrxi struct 10%

--- lib/matrix/includes/matrix.h
+++ lib/matrix/includes/matrix.h
@@ -4,11 +4,19 @@
 #ifndef FT_MATRIX_H
 #define FT_MATRIX_H
 #include <math.h>
-#include "../../arraylist/includes/array_list.h"
-#include "try-catch.h"
+#include "array_list.h"
+#include "try_catch.h"
 typedef struct s_matrix
 {
-  
+  double **data;
+  int cols;
+  int rows;
+  void (*add)(struct s_matrix *this, double n);
+  void (*add_matrix)(struct s)
+  void (*sub)(struct s_matrix *this, double n);
 }             t_matrix;
 
+t_matrix *new_matrix(int row, int cols);
+t_matrix *from_array(double array, int array_size);
+t_matrix *from_array_list(t_array_list array);
 #endif
