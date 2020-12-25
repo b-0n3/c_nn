711dbe6419f2d06f74056a18896739a370792e10
lalouabd <lalouabd123@gmail.com>
2020-12-25 17:03:55 +0100

    delete cache

---
+++ lib/arraylist/tests/test.c
@@ -0,0 +1,15 @@
+#include "array_list.h"
+
+t_array_list *array;
+jmp_buf * __TRY_CATCH_LIB__raise_env = NULL;
+jmp_buf * __TRY_CATCH_LIB__retry_env = NULL;
+void test_new_array()
+{
+   new_array_list(array ,4,sizeof(int));
+  if (array == NULL)
+    RAISE(NULL_POINTER_EXCEPTION);
+}
+
+int main(){
+
+}
