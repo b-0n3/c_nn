8addb03844ca623b2ad37a75bf23ecfe0d099195
lalouabd <lalouabd123@gmail.com>
2021-02-17 02:40:52 +0100

    impliment forEach and forEeachIndex

--- lib/matrix/srcs/matrix_for_each.c
+++ lib/matrix/srcs/matrix_for_each.c
@@ -2,12 +2,18 @@
 
 void matrix_for_each_i(t_matrix *this , void (*f)(double var , int row_idex , int col_index))
 {
-  for (int i = 0; i < this->rows; i++)
-    for (int j = 0; i < this->cols; j++)
-        f(this->data[i][j], i, j);
+	if (this == NULL || f == NULL)
+		RAISE(NULL_POINTER_EXCEPTION);
+	for (int i = 0; i < this->rows; i++)
+		for (int j = 0; j < this->cols; j++)
+				f(this->data[i][j], i, j);
 }
 
 void matrix_for_each(t_matrix *this, void (*f)(double var))
 {
-
+	 if (this == NULL || f == NULL)
+		RAISE(NULL_POINTER_EXCEPTION);
+	for (int i = 0; i < this->rows; i++)
+		for (int j = 0; j < this->cols; j++)
+				f(this->data[i][j]);
 }
