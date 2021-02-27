6b64700f256ff010bebf9634ce4ec55c2ce56b68
cRYP70n-13 <otmane.kimdil@gmail.com>
2021-02-27 23:58:52 +0100

    Refactoring some code because it looks ugly

--- lib/matrix/srcs/matrix_init.c
+++ lib/matrix/srcs/matrix_init.c
@@ -1,6 +1,5 @@
 #include "matrix.h"
 
-
 extern jmp_buf * __TRY_CATCH_LIB__raise_env;
 // used by macros for RETRY
 extern jmp_buf * __TRY_CATCH_LIB__retry_env;
@@ -33,23 +32,26 @@ void init_matrix(t_matrix *matrix, int row, int cols)
 	matrix->to_array = &matrix_to_array;
 	matrix->free = &free_matrix;
 }
- // ! @see lib/matrix/includes/matrix.h
+
+// ! @see lib/matrix/includes/matrix.h
 // @return new matrix 
 // ! @throws ILLEGAL_ARGUMENT_EXCEPTION if row < 1 || cols < 1
 // ! @throws MALLOC_EXCEPTION
 t_matrix *new_matrix(int row, int cols){
-    if (row < 1 || cols < 1)
-        RAISE(ILLEGAL_ARGUMENT_EXCEPTION);
-    t_matrix *matrix =(t_matrix *) rmalloc(sizeof(t_matrix));
+	if (row < 1 || cols < 1)
+		RAISE(ILLEGAL_ARGUMENT_EXCEPTION);
+	t_matrix *matrix =(t_matrix *) rmalloc(sizeof(t_matrix));
 	init_matrix(matrix, row, cols);
 }
+
 //@retrun new matrix with random values between -1 and 1
 t_matrix *new_matrix_random_values(int row, int cols){
-  t_matrix *matrix;
-  matrix = new_matrix(row, cols);
-  for(int i = 0; i < matrix->rows; i++)
-    for(int j = 0; j < matrix->cols; j++)
-      matrix->put(matrix , i , j, frandom());
+	t_matrix *matrix;
+
+	matrix = new_matrix(row, cols);
+	for (int i = 0; i < matrix->rows; i++)
+		for (int j = 0; j < matrix->cols; j++)
+			matrix->put(matrix , i , j, frandom());
 }
 
 // @param array :data
@@ -60,14 +62,14 @@ t_matrix *new_matrix_random_values(int row, int cols){
 // ! @throws ILLEGAL_ARGUMENT_EXCEPTION if array == NULL || array_size <=0
 // ! @throws MALLOC_EXCEPTION
 t_matrix *from_array(double *array, int array_size){
-  t_matrix *matrix;
+	t_matrix *matrix;
 
-  if (array == NULL)
-    RAISE(NULL_POINTER_EXCEPTION);
-  matrix = new_matrix(1,array_size);
-  for (int i = 0; i < array_size; i++)
-    matrix->put(matrix, 0, i, array[i]);
-  return matrix;
+	if (array == NULL)
+		RAISE(NULL_POINTER_EXCEPTION);
+	matrix = new_matrix(1,array_size);
+	for (int i = 0; i < array_size; i++)
+		matrix->put(matrix, 0, i, array[i]);
+	return matrix;
 }
 
 // ! @see  /lib/arraylist/includes/array_list.h 
@@ -80,17 +82,13 @@ t_matrix *from_array(double *array, int array_size){
 // ! @throws ILLEGAL_ARGUMENT_EXCEPTION if  array.index <= 0
 // ! @throws MALLOC_EXCEPTION
 t_matrix *from_array_list(t_array_list array){
-    t_matrix *matrix;
+	t_matrix *matrix;
 
-    if (array.arr == NULL)
-      RAISE(NULL_POINTER_EXCEPTION);
-    if (array.size_of_object != sizeof(double))
-      RAISE(ILLEGAL_ARGUMENT_EXCEPTION);
-    matrix = new_matrix(1, array.index);
-    for (int i = 0; i < array.index; i++)
-      matrix->put(matrix, 0, i, *((double *)(array.get(&array, i))));
+	if (array.arr == NULL)
+		RAISE(NULL_POINTER_EXCEPTION);
+	if (array.size_of_object != sizeof(double))
+		RAISE(ILLEGAL_ARGUMENT_EXCEPTION);
+	matrix = new_matrix(1, array.index);
+	for (int i = 0; i < array.index; i++)
+		matrix->put(matrix, 0, i, *((double *)(array.get(&array, i))));
 }
-
-
-
-
